c6475f03ccb719fda3f44ae63100397bec31edbf
wwwmail.cz <www@mail.cz>
2019-11-23 17:06:37 +0200

    init

---
+++ catalog/language/ru-ru/common/search.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_search'] = 'Поиск по сайту';
+$_['text_search_btn'] = 'Поиск';
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fordevelopment.info/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fordevelopment.info/');
+
+// DIR
+define('DIR_APPLICATION', '/home/h31317c/public_html/catalog/');
+define('DIR_SYSTEM', '/home/h31317c/public_html/system/');
+define('DIR_IMAGE', '/home/h31317c/public_html/image/');
+define('DIR_STORAGE', '/home/h31317c/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'h31317c_dev');
+define('DB_PASSWORD', '9*k#-=SlVB&k');
+define('DB_DATABASE', 'h31317c_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
